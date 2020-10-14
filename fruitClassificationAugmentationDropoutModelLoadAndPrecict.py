88efcefda9f27c56b0dfaae402653d451cc8ed53
Thilo <thilo.umland@gmail.com>
2020-10-14 20:57:29 +0200

    second commit

---
+++ fruitClassificationAugmentationDropoutModelLoadAndPrecict.py
@@ -0,0 +1,47 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'  # 0 => log everything; 1 => no INFO; 2 => no INFO/WARNINGS; 3 => no INFO/WARNINGS/ERRORS
+
+import PIL
+import tensorflow as tf
+import pathlib
+from operator import itemgetter
+
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.models import Sequential
+
+img_height = 100
+img_width = 100
+
+data_dir = "./trainingSmaller"
+predictionPath = "./predictionPhotos/appleOwn.jpg"
+
+classes = tf.keras.preprocessing.image_dataset_from_directory(data_dir)
+class_names = classes.class_names
+
+model = tf.keras.models.load_model('./savedModel')
+
+img = keras.preprocessing.image.load_img(
+    predictionPath, target_size=(img_height, img_width)
+)
+img_array = keras.preprocessing.image.img_to_array(img)
+img_array = tf.expand_dims(img_array, 0) # Create a batch
+
+predictions = model.predict(img_array)
+score = tf.nn.softmax(predictions[0])
+
+print(
+    "This image most likely belongs to {} with a {:.2f} percent confidence."
+    .format(class_names[np.argmax(score)], 100 * np.max(score))
+)
+classAndProb = []
+score = score.numpy()
+for num, name in enumerate(class_names):
+    classAndProb.append([name, "{:.2f}".format(float(score[num])*100)])
+classAndProb = sorted(classAndProb, key=itemgetter(1), reverse=True)
+print("")
+for i in classAndProb:
+    print("Content: {} -- Probability {}%".format(i[0],i[1]) )
