9c76fa172611a64a6aaf2274740d1fa55d134ee9
Thilo <thilo.umland@gmail.com>
2020-10-23 14:44:42 +0200

    stuff

---
+++ ttsTest.py
@@ -0,0 +1,19 @@
+from gtts import gTTS
+
+# tts = gTTS('Das Bild zeigt einen Apfel', lang='de')
+# tts.save('./audio/apple.mp3')
+
+# tts = gTTS('Das Bild zeigt eine Banane', lang='de')
+# tts.save('./audio/banana.mp3')
+
+# tts = gTTS('Das Bild zeigt eine Orange', lang='de')
+# tts.save('./audio/orange.mp3')
+
+# tts = gTTS('Das Bild zeigt eine Tomate', lang='de')
+# tts.save('./audio/tomato.mp3')
+
+# tts = gTTS('Ich bin mir nicht sicher, aber ich denke ', lang='de')
+# tts.save('./audio/notSure.mp3')
+
+tts = gTTS('oder', lang='de')
+tts.save('./audio/or.mp3')
